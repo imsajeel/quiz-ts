5a94ace078caf49e195d026ba0397a83c1e53abe
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-07-29 09:33:06 +0500

    boiler-plate setup

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { type } from "os";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestion: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestion,
+}) => {
+  return (
+    <div>
+      <p>
+        Quistion : {questionNum} / {totalQuestion}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer, index) => (
+          <div key={index}>
+            <button disabled={userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
