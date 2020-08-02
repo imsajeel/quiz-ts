7202f4296251f5d08e58e1c844ca7c9b894982b1
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-08-02 23:50:00 +0500

    add pionter cursor and onClick

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Header() {
   return (
     <header className="bg-blue-500 ">
-      <h1 className="text-gray-100 text-2xl m-3 md:mx-10">uQuizy?</h1>
+      <h1
+        className="text-gray-100 text-2xl m-3 md:mx-10 cursor-pointer"
+        onClick={() => (window.location.href = "/reload")}
+      >
+        uQuizy?
+      </h1>
     </header>
   );
 }
