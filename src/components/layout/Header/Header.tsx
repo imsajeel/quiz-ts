e9b8f2e65e0e76dc8aafc2c9a2f4ee9f3971b76c
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-08-03 00:01:53 +0500

    added github button

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="bg-blue-500 ">
+    <header className="bg-blue-500 flex justify-center items-center">
       <h1
-        className="text-gray-100 text-2xl m-3 md:mx-10 cursor-pointer"
+        className="text-white text-2xl m-3 md:mx-10 cursor-pointer flex-1"
         onClick={() => (window.location.href = "/reload")}
       >
         uQuizy?
       </h1>
+      <div className="text-white text-4xl m-3 md:mx-10 cursor-pointer">
+        <a
+          href="https://github.com/imsajeel/quiz-ts"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+      </div>
     </header>
   );
 }
